e1567c600214d60739c1735dd69eeb7e97dc8f41
kerplow <charlesdegh@gmail.com>
2018-09-23 20:20:49 +0200

    last attempt

--- db/migrate/20180831155035_add_parent_to_note.rb
+++ db/migrate/20180831155035_add_parent_to_note.rb
@@ -1,5 +1,6 @@
 class AddParentToNote < ActiveRecord::Migration[5.0]
   def change
-    add_column :notes, :parent_id, foreign_key: true
+    add_reference :notes, :parent, index: true
+    add_foreign_key :notes, :parent, column: :parent_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20180831155035) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: :cascade do |t|
     t.string   "name"
     t.text     "contents"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "parent_id_id"
-    t.index ["parent_id_id"], name: "index_notes_on_parent_id_id"
+    t.index ["parent_id_id"], name: "index_notes_on_parent_id_id", using: :btree
   end
 
   create_table "planner_events", force: :cascade do |t|
@@ -44,8 +47,8 @@ ActiveRecord::Schema.define(version: 20180831155035) do
     t.string   "room"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
