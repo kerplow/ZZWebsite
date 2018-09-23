da80b0c49b0db0c429ec655a592520d7324b9e12
kerplow <charlesdegh@gmail.com>
2018-09-23 20:52:26 +0200

    almost there

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:email, :password).permit(:first_name, :last_name, :nickname :bio, :room, :phone_number, :email)
+    params.require(:email, :password).permit(:first_name, :last_name, :nickname, :bio, :room, :phone_number, :email)
   end
 end
